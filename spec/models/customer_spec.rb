1bab580a73296de8510d298aa28bf304a7785bee
pierry01 <pierrybm@gmail.com>
2019-06-27 23:42:46 +0000

    Sobrescrevendo atributos e Aliases para as fábricas

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
+  it '#full_name - sobrescrevendo atributo' do
+    customer = build(:user, name:  'Jean Pierry') # ou build(:customer)
+    expect(customer.full_name).to eq('Sr. Jean Pierry')
+  end
+  
   it '#full_name' do
     customer = create(:customer)
     expect(customer.full_name).to start_with('Sr. ')
