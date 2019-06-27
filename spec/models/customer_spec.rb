bf475db195590383e0d4fadb5598ebf18afec997
pierry01 <pierrybm@gmail.com>
2019-06-27 22:34:43 +0000

    Começando com Fixtures

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :customers
+  
+  it 'Create a Customer' do
+    customer = customers(:jean)
+    
+    expect(customer.full_name).to eq('Sr. Jean')
+  end
 end
