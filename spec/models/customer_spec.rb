bdfaf4c064f28452dedff5fd6e76d5a0ca0fdd53
pierry01 <pierrybm@gmail.com>
2019-06-28 00:04:41 +0000

    Atributo Transitório

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -27,5 +27,10 @@ RSpec.describe Customer, type: :model do
     expect(customer.full_name).to start_with('Sr. ')
   end
   
+  it 'Atributo Transitório' do
+    customer = create(:customer, upcased: true)
+    expect(customer.name.upcase).to eq(customer.name)
+  end
+  
   it { expect { create(:customer) }.to change { Customer.all.size }.by(1) }
 end
