6da39e8d827a08e7e716decd40179ee74ebb4ac7
pierry01 <pierrybm@gmail.com>
2019-07-20 12:10:25 +0000

    has_many

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :customer, aliases: [:user] do
     transient do
       upcased { false }
+      qtt_orders { 3 }
     end
   
     name { Faker::Name.name }
@@ -9,19 +10,21 @@ FactoryBot.define do
     vip { false }
     days_to_pay { 15 }
 
-    trait :male do
-      gender { 'M' }
-    end
-    
-    trait :female do
-      gender { 'F' }
-    end
+    trait :male do gender { 'M' } end
+    trait :female do gender { 'F' } end
     
     trait :vip do
       vip { true }
       days_to_pay { 30 }
     end
     
+    trait :with_orders do
+      after(:create) do |customer, evaluator|
+        create_list(:order, evaluator.qtt_orders, customer: customer)
+      end
+    end
+    
+    factory :customer_with_orders, traits: [:with_orders]
     factory :customer_male, traits: [:male]
     factory :customer_female, traits: [:female]
     factory :customer_vip, traits: [:vip]
