b0745a3ca68cbf2d427e5cb4d12ca4a39847d6ff
pierry01 <pierrybm@gmail.com>
2019-07-26 15:03:43 +0000

    webmock

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -2,7 +2,13 @@ require 'httparty'
 require 'factory_bot_rails'
 
 describe 'HTTParty' do
-  it 'HTTParty' do
+  it 'content_type' do
+    stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+      to_return(
+        status: 200, 
+        headers: { 'content-type':'application/json' }
+      )
+         
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
