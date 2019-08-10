7200a4d51b4c57f36b9f5b607bb0fcdb31f92611
pierry01 <pierrybm@gmail.com>
2019-08-10 20:09:46 +0000

    without gem

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Customers', type: :request do
-  describe 'GET /customers' do
+  describe 'GET' do
     it 'works! 200' do
       get customers_path
       expect(response).to have_http_status(200)
@@ -24,9 +24,17 @@ RSpec.describe 'Customers', type: :request do
         email: (be_kind_of String)
       )
     end
+    
+    it '#show JSON without gem' do
+      get '/customers/1.json'
+      response_body = JSON.parse(response.body)
+      expect(response_body.fetch('id')).to eq(1)
+      expect(response_body.fetch('name')).to be_kind_of(String)
+      expect(response_body.fetch('email')).to be_kind_of(String)
+    end
   end
   
-  describe 'POST /customers' do
+  describe 'POST' do
     it '#create JSON' do
       member = create(:member)
       login_as(member, scope: :member)
@@ -41,6 +49,40 @@ RSpec.describe 'Customers', type: :request do
         name: customer_params[:name],
         email: customer_params.fetch(:email)
       )
+    end
+  end
+  
+  describe 'PUT' do
+    it '#update JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+      
+      headers = { 'Accept' => 'application/json' }
+      
+      customer = Customer.first
+      customer.name += ' - ATT'
+      
+      patch "/customers/#{customer.id}.json", params: { customer: customer.attributes }, headers: headers
+      
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: customer.name,
+        email: customer.email
+      )
+    end
+  end
+  
+  describe 'DELETE' do
+    it '#destroy JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+      
+      headers = { 'Accept' => 'application/json' }
+
+      customer = Customer.first
+
+      expect { delete "/customers/#{customer.id}.json", headers: headers }.to change(Customer, :count).by(-1)
+      expect(response).to have_http_status(204)
     end
   end
 end
